37f1de8cc986713ee44ecfd2bd20fb929bc93725
dsun2188 <sveta8@mail.ru>
2021-08-04 20:22:30 +0300

    ДЗ к Урок 2. Java. Основные конструкции

--- src/GB_HW1.java
+++ src/GB_HW1.java
@@ -1,54 +1,62 @@
 public class GB_HW1 {
-
-        // Задание 1
     public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
+
+        System.out.println("Здание 1");
+        int a = 5;
+        int b = 12;
+        boolean isBetween = isBetween10And20(a, b);
+        System.out.println(isBetween);
+
+        System.out.println("Здание 2");
+        isPositiveOrNegative(5);
+
+        System.out.println("Здание 3");
+        boolean isNegative = isNegative( -3);
+        System.out.println(isNegative);
+
+        System.out.println("Здание 4");
+        printWordNTimes("Hello!!!", 6);
+
+        System.out.println("Здание 5");
+
     }
-        //Задание 2
-        public static void printThreeWords () {
-            System.out.println("Orange");
-            System.out.println("Banana");
-            System.out.println("Apple");
+
+    //1. Написать метод, принимающий на вход два целых числа и проверяющий,
+    // что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+    public static boolean isBetween10And20(int left, int right) {
+        int sum = left + right;
+        return 10 < sum && sum <= 20;
+    }
+
+    //2. Написать метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль,
+    // положительное ли число передали или отрицательное. Замечание: ноль считаем положительным числом.
+    public static void isPositiveOrNegative(int number) {
+        if (number >= 0) {
+            System.out.println("Положительное");
         }
-
-        //Задание 3
-        public static void checkSumSign () {
-            int a = 15;
-            int b = -20;
-            int sum = a + b;
-
-            if (sum >= 0) {
-                System.out.println("Сумма положительная");
-            } else {
-                System.out.println("Сумма отрицательная");
-            }
-        }
-
-        //Задание 4
-        public static void printColor () {
-            int value = 170;
-
-            if (value <= 0) {
-                System.out.println("Красный");
-            } else if (value > 0 & value <= 100) {
-                System.out.println("Желтый");
-            } else {
-                System.out.println("Зеленый");
-            }
-        }
-
-        //Задание 5
-        public static void compareNumbers () {
-            int a = 10;
-            int b = 5;
-
-            if (a >= b) {
-                System.out.println("a >= b");
-            } else {
-                System.out.println("a < b");
-            }
+        else {
+            System.out.println("Отрицательное");
         }
     }
+    //3. Написать метод, которому в качестве параметра передается целое число.
+    //Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+    public static boolean isNegative(int number) {
+        if (number < 0) {
+            return true;
+        }
+            return false;
+    }
+    //4. Написать метод, которому в качестве аргументов передается строка и число, метод должен
+    //отпечатать в консоль указанную строку, указанное количество раз
+    public static void printWordNTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(word);
+        }
+    }
+    //5. * Написать метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true,
+    //не високосный - false). Каждый 4-й год является високосным, кроме каждого 100-го,
+    //при этом каждый 400-й – високосный.
+}
+
+
+
