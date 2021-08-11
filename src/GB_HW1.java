8580c1bb0dbcb0f9a5ee934b8b1069fd138a03e6
dsun2188 <sveta8@mail.ru>
2021-08-11 15:18:16 +0300

    ДЗ к Урок 4. Java. Крестики-нолики в процедурном стиле

--- src/GB_HW1.java
+++ src/GB_HW1.java
@@ -1,128 +1,122 @@
-import java.util.Arrays;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class GB_HW1 {
-    public static void main(String[] args) {
-        valueReplacement();
-        fillArray();
-        valueReplacement2();
-        fillDiagonal();
-        System.out.println(Arrays.toString(fillIntValueArray(3, 10)));
 
-        int[] arr = {2, 2, 3, 1, 2, 2, 10, 2};
-        System.out.println(compareAmounts(arr));
-    }
+    //1.Полностью разобраться с кодом, попробовать переписать с нуля, стараясь
+    // не подглядывать в методичку;
+    //2.Переделать проверку победы, чтобы она не была реализована просто набором условий,
+    // например, с использованием циклов.
+    //3.*Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и
+    //количества фишек 4. Очень желательно не делать это просто набором условий для каждой из
+    // возможных ситуаций;
+    // 4.***Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
 
-    public static boolean compareAmounts(int[] arr) {
-        int sumLeftArray = 0, sumArray = 0;
+        public static int SIZE = 5;
+        public static int DOTS_TO_WIN = 5;
 
-        for (int i = 0; i < arr.length; i++) {
-            sumArray += arr[i];
+        public static final char DOT_EMPTY = '•';
+        public static final char DOT_X = 'X';
+        public static final char DOT_O = 'O';
+        public static char[][] map;
+        public static Scanner sc = new Scanner(System.in);
+        public static Random rand = new Random();
+        public static void main(String[] args) {
+            initMap();
+            printMap();
+            while (true) {
+                humanTurn();
+                printMap();
+                if (checkWin(DOT_X)) {
+                    System.out.println("Победил человек");
+                    break;
+                }
+                if (isMapFull()) {
+                    System.out.println("Ничья");
+                    break;
+                }
+                aiTurn();
+                printMap();
+                if (checkWin(DOT_O)) {
+                    System.out.println("Победил Компьютер");
+                    break;
+                }
+                if (isMapFull()) {
+                    System.out.println("Ничья");
+                    break;
+                }
+            }
+            System.out.println("Игра закончена");
         }
-
-        for (int j = 0; j < arr.length; j++) {
-            sumLeftArray += arr[j];
-            if (sumLeftArray == sumArray - sumLeftArray) break;
+        public static boolean checkWin(char symb) {
+            // horizontal winner checkup
+            if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb && map[0][3] == symb && map[0][4] == symb) return true;
+            if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb && map[1][3] == symb && map[1][4] == symb) return true;
+            if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb && map[2][3] == symb && map[2][4] == symb) return true;
+            if (map[3][0] == symb && map[3][1] == symb && map[3][2] == symb && map[3][3] == symb && map[3][4] == symb) return true;
+            if (map[4][0] == symb && map[4][1] == symb && map[4][2] == symb && map[4][3] == symb && map[4][4] == symb) return true;
+            //  diagonal winner checkup
+            if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb && map[3][3] == symb && map[4][4] == symb) return true;
+            //vertical winner checkup
+            if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb && map[3][0] == symb && map[4][0] == symb) return true;
+            if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb && map[3][1] == symb && map[4][1] == symb) return true;
+            if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb && map[3][2] == symb && map[4][2] == symb) return true;
+            if (map[0][3] == symb && map[1][3] == symb && map[2][3] == symb && map[3][3] == symb && map[4][3] == symb) return true;
+            if (map[0][4] == symb && map[1][4] == symb && map[2][4] == symb && map[3][4] == symb && map[4][4] == symb) return true;
+            return false;
         }
-        return (sumLeftArray == sumArray - sumLeftArray);
-    }
-
-    public static int[] fillIntValueArray(int len, int initialValue) {
-        int[] arr = new int[len];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = initialValue;
+        public static boolean isMapFull() {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (map[i][j] == DOT_EMPTY) return false;
+                }
+            }
+            return true;
         }
-        return arr;
-    }
-
-
-    public static void fillDiagonal() {
-        final int SIZE = 4;
-        int n = 0;
-        int[][] arr = new int[SIZE][SIZE];
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (i == j) arr[i][j] = 1;
-                if ((i + j) == SIZE - 1) arr[i][j] = 1;
+        public static void aiTurn() {
+            int x, y;
+            do {
+                x = rand.nextInt(SIZE);
+                y = rand.nextInt(SIZE);
+            } while (!isCellValid(x, y));
+            System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+            map[y][x] = DOT_O;
+        }
+        public static void humanTurn() {
+            int x, y;
+            do {
+                System.out.println("Введите координаты в формате X -диаг. Y-вертик.");
+                x = sc.nextInt() - 1;
+                y = sc.nextInt() - 1;
+            } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+            map[y][x] = DOT_X;
+        }
+        public static boolean isCellValid(int x, int y) {
+            if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+            if (map[y][x] == DOT_EMPTY) return true;
+            return false;
+        }
+        public static void initMap() {
+            map = new char[SIZE][SIZE];
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    map[i][j] = DOT_EMPTY;
+                }
             }
         }
-        //проверка. можно убрать.
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.printf("%3d ", arr[i][j]);
+        public static void printMap() {
+            for (int i = 0; i <= SIZE; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < SIZE; i++) {
+                System.out.print((i + 1) + " ");
+                for (int j = 0; j < SIZE; j++) {
+                    System.out.print(map[i][j] + " ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
-
     }
-
-    public static void valueReplacement2() {
-        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < 6) arr[i] *= 2;
-        }
-    }
-
-    private static void fillArray() {
-        final int SIZE = 100;
-        int[] arr = new int[SIZE];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i + 1;
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-
-    }
-
-    public static void valueReplacement() {
-        final int SIZE = 10;
-        int[] arr = {1, 0, 0, 1, 1, 1, 0, 0, 1, 0};
-        System.out.println(Arrays.toString(arr));
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) arr[i] = 0;
-            else arr[i] = 1;
-        }
-        System.out.println(Arrays.toString(arr));
-
-
-    }
-}
-    //1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-    //С помощью цикла и условия заменить 0 на 1, 1 на 0;
-
-    //2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
-
-    //3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
-
-    //4. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
-    //и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из
-    // диагоналей, если обе сложно). Определить элементы одной из диагоналей можно по следующему
-    // принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n];
-
-    //5. Написать метод, принимающий на вход два аргумента: len и initialValue,
-    //и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue;
-
-    //6. * Задать одномерный массив и найти в нем минимальный и максимальный элементы ;
-
-    //7. ** Написать метод, в который передается не пустой одномерный целочисленный массив, метод должен вернуть true,
-    //если в массиве есть место, в котором сумма левой и правой части массива равны.
-    //**Примеры:
-    //checkBalance([2, 2, 2, 1, 2, 2, ||| 10, 1]) → true, т.е. 2 + 2 + 2 + 1 + 2 + 2 = 10 + 1
-    //checkBalance([1, 1, 1, ||| 2, 1]) → true, т.е. 1 + 1 + 1 = 2 + 1
-    //
-    //граница показана символами |||, эти символы в массив не входят и не имеют никакого отношения к ИЛИ.
-
-
-    //8. *** Написать метод, которому на вход подается одномерный массив и число n (может быть положительным,
-    //или отрицательным), при этом метод должен сместить все элементы массива на n позиций. Элементы смещаются
-    //циклично. Для усложнения задачи нельзя пользоваться вспомогательными массивами.
-    //Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1]
-    //при n = -2 (на два влево) -> [ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг можете выбирать сами.
-
-
-
-
-
-
