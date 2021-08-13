4142afe4143f6b068938ba09a8ffd88013507f70
dsun2188 <sveta8@mail.ru>
2021-08-14 00:12:01 +0300

    ДЗ к Урок 5. Java. Введение в ООП

--- src/GB_HW1.java
+++ src/GB_HW1.java
@@ -1,122 +1,66 @@
-
-import java.util.Random;
-import java.util.Scanner;
-
 public class GB_HW1 {
+    public static void main(String[] args) {
 
-    //1.Полностью разобраться с кодом, попробовать переписать с нуля, стараясь
-    // не подглядывать в методичку;
-    //2.Переделать проверку победы, чтобы она не была реализована просто набором условий,
-    // например, с использованием циклов.
-    //3.*Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и
-    //количества фишек 4. Очень желательно не делать это просто набором условий для каждой из
-    // возможных ситуаций;
-    // 4.***Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока.
+        //1. Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст.
+        //2. Конструктор класса должен заполнять эти поля при создании объекта.
+        //3. Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль.
+        //4. Создать массив из 5 сотрудников.
+        //Пример:
+        //Person[] persArray = new Person[5]; // Вначале объявляем массив объектов
+        //persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        // потом для каждой ячейки массива задаем объект
+        //persArray[1] = new Person(...);
+        //...
+        //persArray[4] = new Person(...);
+        //5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
 
-        public static int SIZE = 5;
-        public static int DOTS_TO_WIN = 5;
+        Worker[] worker =
+                {
+                        new Worker(
+                                "Сидоров Иван Петрович",
+                                "Бухгалтер",
+                                "sid@mail.ru",
+                                "7-777-77-77",
+                                80000,
+                                33),
+                        new Worker(
+                                "Иванов Дмитрий Петрович",
+                                "Менеджер",
+                                "iv@mail.ru",
+                                "7-777-77-88",
+                                85000,
+                                35),
+                        new Worker(
+                                "Казаков Владимир Иванович",
+                                "Младший Менеджер",
+                                "kaz@mail.ru",
+                                "7-777-77-99",
+                                75000,
+                                30),
+                        new Worker(
+                                "Мухин Дмитрий Львович",
+                                "Старший Менеджер",
+                                "mu@mail.ru",
+                                "7-777-77-66",
+                                95000,
+                                38),
+                        new Worker(
+                                "Щека Клавдия Петровна",
+                                "Уборщица",
+                                "ch@mail.ru",
+                                "7-777-77-55",
+                                59000,
+                                40)
+                };
 
-        public static final char DOT_EMPTY = '•';
-        public static final char DOT_X = 'X';
-        public static final char DOT_O = 'O';
-        public static char[][] map;
-        public static Scanner sc = new Scanner(System.in);
-        public static Random rand = new Random();
-        public static void main(String[] args) {
-            initMap();
-            printMap();
-            while (true) {
-                humanTurn();
-                printMap();
-                if (checkWin(DOT_X)) {
-                    System.out.println("Победил человек");
-                    break;
-                }
-                if (isMapFull()) {
-                    System.out.println("Ничья");
-                    break;
-                }
-                aiTurn();
-                printMap();
-                if (checkWin(DOT_O)) {
-                    System.out.println("Победил Компьютер");
-                    break;
-                }
-                if (isMapFull()) {
-                    System.out.println("Ничья");
-                    break;
-                }
-            }
-            System.out.println("Игра закончена");
-        }
-        public static boolean checkWin(char symb) {
-            // horizontal winner checkup
-            if(map[0][0] == symb && map[0][1] == symb && map[0][2] == symb && map[0][3] == symb && map[0][4] == symb) return true;
-            if(map[1][0] == symb && map[1][1] == symb && map[1][2] == symb && map[1][3] == symb && map[1][4] == symb) return true;
-            if (map[2][0] == symb && map[2][1] == symb && map[2][2] == symb && map[2][3] == symb && map[2][4] == symb) return true;
-            if (map[3][0] == symb && map[3][1] == symb && map[3][2] == symb && map[3][3] == symb && map[3][4] == symb) return true;
-            if (map[4][0] == symb && map[4][1] == symb && map[4][2] == symb && map[4][3] == symb && map[4][4] == symb) return true;
-            //  diagonal winner checkup
-            if (map[0][0] == symb && map[1][1] == symb && map[2][2] == symb && map[3][3] == symb && map[4][4] == symb) return true;
-            //vertical winner checkup
-            if (map[0][0] == symb && map[1][0] == symb && map[2][0] == symb && map[3][0] == symb && map[4][0] == symb) return true;
-            if (map[0][1] == symb && map[1][1] == symb && map[2][1] == symb && map[3][1] == symb && map[4][1] == symb) return true;
-            if (map[0][2] == symb && map[1][2] == symb && map[2][2] == symb && map[3][2] == symb && map[4][2] == symb) return true;
-            if (map[0][3] == symb && map[1][3] == symb && map[2][3] == symb && map[3][3] == symb && map[4][3] == symb) return true;
-            if (map[0][4] == symb && map[1][4] == symb && map[2][4] == symb && map[3][4] == symb && map[4][4] == symb) return true;
-            return false;
-        }
-        public static boolean isMapFull() {
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    if (map[i][j] == DOT_EMPTY) return false;
-                }
-            }
-            return true;
-        }
-        public static void aiTurn() {
-            int x, y;
-            do {
-                x = rand.nextInt(SIZE);
-                y = rand.nextInt(SIZE);
-            } while (!isCellValid(x, y));
-            System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
-            map[y][x] = DOT_O;
-        }
-        public static void humanTurn() {
-            int x, y;
-            do {
-                System.out.println("Введите координаты в формате X -диаг. Y-вертик.");
-                x = sc.nextInt() - 1;
-                y = sc.nextInt() - 1;
-            } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
-            map[y][x] = DOT_X;
-        }
-        public static boolean isCellValid(int x, int y) {
-            if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-            if (map[y][x] == DOT_EMPTY) return true;
-            return false;
-        }
-        public static void initMap() {
-            map = new char[SIZE][SIZE];
-            for (int i = 0; i < SIZE; i++) {
-                for (int j = 0; j < SIZE; j++) {
-                    map[i][j] = DOT_EMPTY;
-                }
-            }
-        }
-        public static void printMap() {
-            for (int i = 0; i <= SIZE; i++) {
-                System.out.print(i + " ");
-            }
-            System.out.println();
-            for (int i = 0; i < SIZE; i++) {
-                System.out.print((i + 1) + " ");
-                for (int j = 0; j < SIZE; j++) {
-                    System.out.print(map[i][j] + " ");
-                }
+                int minAge = 30;
+        for (Worker worker1 : worker)
+        {
+            if (worker1.age > minAge)
+            {
+                worker1.printInfo();
                 System.out.println();
             }
-            System.out.println();
         }
     }
+}
